5c872154aee1bd774bbe44845736ff69627a4804
vaibhavyxx <vd5443@rit.edu>
2024-08-07 21:51:01 +0530

    added bird and plane animation with it detecting birds collision

--- runner game/main-v2.py
+++ runner game/main-v2.py
@@ -1,4 +1,5 @@
 #changed the idea to just jumping on the sky and avoiding grey clouds
+#to do- add event management, scoring, ui buttons, sound
 import pygame
 import random
 import time
@@ -11,10 +12,22 @@ pygame.mouse.set_visible(False)
 clock = pygame.time.Clock()
 start_time = pygame.time.get_ticks()
 
+#loads all the assets
+bird_fly = []
+bird_fly.append(pygame.image.load("runner game/png/PNG/SMALL/BirdS_0000_Capa-1.png"))
+bird_fly.append(pygame.image.load("runner game/png/PNG/SMALL/BirdS_0001_Capa-2.png"))
+bird_fly.append(pygame.image.load("runner game/png/PNG/SMALL/BirdS_0002_Capa-3.png"))
+bird_fly.append(pygame.image.load("runner game/png/PNG/SMALL/BirdS_0003_Capa-0.png"))
+plane_fly = []
+plane_fly.append(pygame.image.load("runner game/png/PNG/Plane/Fly (1).png"))
+plane_fly.append(pygame.image.load("runner game/png/PNG/Plane/Fly (2).png"))
+plane_dead = pygame.image.load("runner game/png/PNG/Plane/Dead (1).png")
+
 class Plane(pygame.sprite.Sprite):
-    def __init__(self, picture_path, scale, gravity):
+    def __init__(self, plane_fly_list, scale, gravity):
         super().__init__()
-        self.image = pygame.image.load(picture_path)
+        self.current_frame =0
+        self.image = plane_fly_list[self.current_frame]
         self.new_width = scale * self.image.get_width()
         self.new_height = scale * self.image.get_height()
         self.image = pygame.transform.scale(self.image, (self.new_width, self.new_height))
@@ -23,15 +36,36 @@ class Plane(pygame.sprite.Sprite):
         self.pos_y = HEIGHT/2 - self.image.get_height()/2
         self.rect.topleft = [self.pos_x, self.pos_y]
         self.gravity = gravity
-        self.ti = time.time()
+        self.ti = time.time()                           #for movement
+        self.start_time = time.time()                   #for animation
 
-#uses physics equation: h = ut +1/2gt^2 where u =0m/s for gravity and falls down when there is no input
+#uses physics equation: h = ut for gravity and falls down when there is no input
     def update(self):
+        self.plane_animation()
+
+        vert_speed = 0
+        keys = pygame.key.get_pressed()
         self.tf = time.time()
         dt = self.tf - self.ti
-        self.pos_y += 0.5 * self.gravity * dt *dt
+
+        if keys[pygame.K_w]:
+            vert_speed = 1000
+        self.pos_y -= vert_speed * dt
+        self.pos_y -= self.gravity * dt 
         self.ti = self.tf
         self.rect.y = self.pos_y
+
+    #shows bird animation
+    def plane_animation(self):
+        current_time = time.time()
+        elapsed_time = current_time - self.start_time
+ 
+        if elapsed_time > 0.5:  
+            self.current_frame += 1
+            self.start_time = current_time
+            if self.current_frame >= len(plane_fly):
+                self.current_frame = 0
+        self.image = plane_fly[self.current_frame]
 
 #cloud class
 class Cloud(pygame.sprite.Sprite):
@@ -85,16 +119,81 @@ class Clouds_Manager(pygame.sprite.Sprite):
     def draw(self, screen):
         self.clouds_group.draw(screen)
 
+#for birds
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, bird_fly_list, pos_x, pos_y, scale, vel):
+        super().__init__()
+        self.current_frame = 0
+        self.image = bird_fly_list[self.current_frame]
+        self.new_width = scale * self.image.get_width()
+        self.new_height = scale * self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (self.new_width, self.new_height))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.velocity = vel
+        self.ti = time.time()                #for movement
+        self.start_time = time.time()        #for animation
+
+#uses physics equation for linear motion .i.e, displacement = vel *time and kills itself if you go offscreen
+    def update(self):
+        self.bird_animation()
+
+        tf = time.time()
+        dt = tf - self.ti
+        self.ti = tf
+        self.pos_x += self.velocity * dt
+        self.rect.x = self.pos_x
+
+        if self.rect.x < -200:
+            self.kill()
+
+    #shows bird animation
+    def bird_animation(self):
+        current_time = time.time()
+        elapsed_time = current_time - self.start_time
+ 
+        if elapsed_time > 0.1:  
+            self.current_frame += 1
+            self.start_time = current_time
+            if self.current_frame >= len(bird_fly):
+                self.current_frame = 0
+        self.image = bird_fly[self.current_frame]
+
+#instantiating cloud- making a manager class to decide its size, position, color, etc.
+class Bird_Manager(pygame.sprite.Sprite):
+    def __init__(self, bird_fly_list):
+        super().__init__()
+        self.bird_fly_list = bird_fly_list
+        self.bird_group = pygame.sprite.Group()
+
+    #randomly gives coordinates, speed and number of birds
+    def make_Birds(self, screen_width, screen_height):
+        num_of_birds = random.randrange(0, 6)
+        for i in range(num_of_birds):
+            x = random.randrange(screen_width, screen_width + 200)      
+            y = random.randrange(0, screen_height -50)                 
+            speed = random.randrange(-700, -400)
+            bird = Bird(self.bird_fly_list, x, y, 1, speed)
+            self.bird_group.add(bird)
+
+    def update(self):
+        self.bird_group.update()
+
+    def draw(self, screen):
+        self.bird_group.draw(screen)
 
 #objects
 cloud_manager = Clouds_Manager("runner game/png/white cloud.png", "runner game/png/grey cloud.png")
 cloud_manager.make_Clouds(WIDTH, HEIGHT, 5)
-player = Plane("runner game/png/plane.png",0.5, 5000)
+player = Plane(plane_fly,0.01, -200)
 player_group = pygame.sprite.Group()
 player_group.add(player)
+bird_manager = Bird_Manager(bird_fly)
+cloud_manager.make_Clouds(WIDTH, HEIGHT, 5)
 
 run = True
-
 #fills the screen with new clouds after every 2 seconds
 while run:
     current_time = pygame.time.get_ticks()
@@ -103,6 +202,7 @@ while run:
     if(elapsed_time > 2000):
         num = random.randrange(0, 5)
         cloud_manager.make_Clouds(WIDTH, HEIGHT, num)
+        bird_manager.make_Birds(WIDTH, HEIGHT)
         start_time = current_time
 
     #update background
@@ -112,12 +212,17 @@ while run:
     cloud_manager.draw(screen)
     player_group.update()
     player_group.draw(screen)
+    bird_manager.update()
+    bird_manager.draw(screen)
 
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        
+        if pygame.sprite.spritecollide(player, bird_manager.bird_group, False):
+            #run = False
+            pass
             
-
     pygame.display.update()
 pygame.quit()
